40203093447e3bd7c0dbba56c0675860090614d2
akanemidori <akane_green@outlook.jp>
2024-01-13 15:52:41 +0900

    新規追加

---
+++ CSharpOdai/CSharpOdai_20240101/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+Console.WriteLine("日付を「MM-dd-yyyy」形式で入力してください。");
+
+var inputtext = Console.ReadLine();
+
+var datecheck = DateTime.TryParseExact(inputtext, "MM-dd-yyyy", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.None, out var dt);
+
+if (datecheck)
+{
+    Console.WriteLine(dt.ToString("変換しました：yyyy/MM/dd"));
+}
+else
+{
+    Console.WriteLine("変換できませんでした。");
+}
+
+Console.WriteLine("このウィンドウを閉じるには、任意のキーを押してください...");
+
+Console.ReadKey();
